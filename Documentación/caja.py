5dd9791ab9a18ade8ac990505aca6af1ff1e045f
Antonio <antoniopgarcia95@gmail.com>
2022-04-25 17:59:09 +0200

    Secciones Hardware

--- Documentación/caja.py
+++ Documentación/caja.py
@@ -1,1 +1,8 @@
-import pdf
+from PyPDF2 import PdfFileReader
+from pdf2jpg import pdf2jpg
+
+inputpath = "CAJA1.pdf"
+outputpath = "output"
+# To convert single page
+result = pdf2jpg.convert_pdf2jpg(inputpath, outputpath, dpi=400, pages="2")
+print(result)
